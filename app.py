25de4af8653f662d3502e31a21a93500a61daf82
6zeropk <nemm3623@naver.com>
2025-04-09 10:36:28 +0900

    로그인/ 회원가입 및 게시물 작성 로직

--- app.py
+++ app.py
@@ -1,41 +1,90 @@
-import pymysql
-from flask import Flask, render_template, request, flash, redirect, url_for
-from pymysql import cursors
+import secrets
+import models
+
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
 
-db_connection = pymysql.connect(
-    host='localhost',
-    user='root',
-    passwd='',
-    database='Board',
-    charset='utf8mb4',
-    cursorclass=cursors.DictCursor
-)
+app.secret_key = secrets.token_hex(32)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    boards = models.get_allBoards()
+    print(boards)
+    return render_template('index.html', boards=boards)
 
+
+############ 로그인 ##############
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+
     elif request.method == 'POST':
+
         id = request.form['id']
         password = request.form['pw']
-        cursor = db_connection.cursor()
 
-        cursor.execute(f'SELECT * FROM users WHERE id={id} and password={password}')
-        user = cursor.fetchone()
+        user = models.get_User(id, password)
 
         if user is None:
-            flash('ID 혹은 Password가 올바르지 않습니다.')
-            #return redirect(url_for('login'))
+            error = '존재하지 않는 계정입니다.'
+            return render_template('login.html', error=error)
 
-        return render_template('login.html')
+        session['user'] = user
 
+        return redirect(url_for('index'))
+
+############### 회원가입 ###############
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+
+    elif request.method == 'POST':
+
+        id = request.form['id']
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['pw']
+
+        if not id or not email or not name or not password:
+            error = "빈칸을 채워주세요."
+            return render_template('register.html', error=error)
+
+        result = models.get_User(id, password)
+
+        if not result:
+            error = '이미 존재하는 아이디입니다.'
+            return render_template("register.html", error=error)
+
+        return redirect(url_for('login'))
+
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == 'GET':
+        return render_template('write.html')
+
+    elif request.method == 'POST':
+
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            error = "제목을 입력해주세요."
+            return render_template('write.html', error=error)
+        elif not content:
+            error = "내용을 입력해주세요."
+            return render_template('write.html', error=error)
+
+        board = {"title": title, "content": content, "id": session['user']["id"]}
+        models.create_Board(board)
+
+        print("clear2")
+
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(port=5000)
---
+++ models.py
@@ -0,0 +1,96 @@
+import pymysql
+from pymysql import cursors
+
+
+def get_connection():
+
+    db_connection = pymysql.connect(
+        host='localhost',
+        user='root',
+        passwd='',
+        database='Board',
+        charset='utf8mb4',
+        cursorclass=cursors.DictCursor
+    )
+
+    return db_connection, db_connection.cursor()
+
+# 모든 게시물
+def get_allBoards() :
+
+    db_connection, cursor = get_connection()
+
+    query = "SELECT * FROM Boards"
+    cursor.execute(query)
+    result = cursor.fetchall()
+
+    cursor.close()
+    db_connection.close()
+
+    return result
+
+# 게시물 찾기
+def get_Board(board_id):
+    db_connection, cursor = get_connection()
+
+    # 튜플이 아닌 문자열이나 정수로 값을 넘기면 excute()는 시퀀스를 요구하기 때문에 에러발생
+    # EX) "abcd" -> 'a', 'b', 'c', 'd' 로 해석
+    query = "SELECT * FROM Boards WHERE id=%s"
+    cursor.execute(query, (board_id,))
+    result = cursor.fetchone()
+
+    cursor.close()
+    db_connection.close()
+
+    return result
+
+def create_Board(board):
+    db_connection, cursor = get_connection()
+
+    query = "INSERT INTO Boards (title, content, id) VALUES (%s, %s, %s)"
+    cursor.execute(query, (board['title'], board['content'], board['id']))
+
+    db_connection.commit()
+
+    cursor.close()
+    db_connection.close()
+
+# 유저 찾기
+def get_User(id, pw):
+    db_connection, cursor = get_connection()
+
+    query = "SELECT * FROM Users WHERE id = %s and password = %s"
+    cursor.execute(query, (id,pw))
+    result = cursor.fetchone()
+
+    cursor.close()
+    db_connection.close()
+
+    return result
+
+# 회원가입
+def register(id, name, email, password):
+    db_connection, cursor = get_connection()
+
+    query = "SELECT * FROM Users WHERE id = %s"
+    cursor.execute(query, (id,))
+    result = cursor.fetchone()
+
+    if result :
+        cursor.close()
+        db_connection.close()
+        return False
+
+    query = "INSERT INTO Users (id, name, email, password) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (id, name, email, password))
+
+    # DB의 변경사항을 진짜 저장, INSERT 혹은 UPDATE는 execute()를 한 것으로는 진짜 저장이 되지 않음
+    db_connection.commit()
+
+    cursor.close()
+    db_connection.close()
+
+    return True
+
+
+
