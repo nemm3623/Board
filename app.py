7aebbcad2923c6a7739dd396f4135ff58e195b0c
6zeropk <nemm3623@naver.com>
2025-04-10 20:19:38 +0900

    게시물 수정/ 삭제 기능 구현

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import secrets
 import models
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 
 app = Flask(__name__)
 
 app.secret_key = secrets.token_hex(32)
 
+
 @app.route('/')
 def index():
     boards = models.get_allBoards()
-    print(boards)
     return render_template('index.html', boards=boards)
 
-############ 로그인 ##############
+
+# 로그인
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
@@ -30,11 +31,13 @@ def login():
             error = '존재하지 않는 계정입니다.'
             return render_template('login.html', error=error)
 
+        # 로그인시 세션 생성
         session['user'] = user
 
         return redirect(url_for('index'))
 
-############### 회원가입 ###############
+
+# 회원가입
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -59,7 +62,8 @@ def register():
 
         return redirect(url_for('login'))
 
-################## 게시물 작성 ###################
+
+# 게시물 작성
 @app.route('/write', methods=['GET', 'POST'])
 def write():
     if request.method == 'GET':
@@ -70,37 +74,67 @@ def write():
         title = request.form['title']
         content = request.form['content']
 
-        if not title:
-            error = "제목을 입력해주세요."
-            return render_template('write.html', error=error)
-        elif not content:
-            error = "내용을 입력해주세요."
-            return render_template('write.html', error=error)
+        if session.get('user') is None:
+            flash("로그인 후 이용해주세요.")
+            return redirect(url_for('login'))
 
-        print(type(session['user']))
+        if not title or not content:
+            flash("빈칸을 채워주세요.")
+            return redirect(url_for('write'))
+
 
         models.create_Board(title, content, session['user']['id'])
 
         return redirect(url_for('index'))
 
-################## 게시물 접속 ###################
-@app.route('/post/<int:No>', methods=['GET'])
+
+# 게시물 방문
+@app.route('/post/<int:No>', methods=['GET','POST'])
 def post(No):
     if request.method == 'GET':
-        board = models.get_Board(No)
+        board = models.get_board(No)
+        models.update_views(No)
 
-        print(request.cookies.get('user'), session['user'])
+        # 요청을 보낸 클라이언트의 세션을 획득
+        is_author = session.get('user')
 
-        #if request.cookies.get('user') == session['user']:
+        # 클라이언트의 id가 작성자의 id와 같은지 확인
+        if is_author and is_author['id'] == session['user']['id']:
+            return render_template('post.html', board=board, author=True)
+        else:
+            return render_template("post.html", board=board)
 
+    elif request.method == 'POST':
 
-        return render_template("post.html", board=board)
+        edit = request.form.get('edit')
 
-    #elif request.method == 'POST':
-
-
-
+        if (edit == "수정"):
+            return redirect(url_for('update', No=No))
+        elif (edit == "삭제"):
+            models.delete_Board(No)
+            return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
     app.run(port=5000)
+
+
+@app.route('/update/<int:No>', methods=['GET', 'POST'])
+def update(No):
+    if request.method == 'GET':
+        board = models.get_board(No)
+        return render_template('update.html', board=board)
+
+    elif request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            error = "제목을 입력해주세요."
+            return render_template('update.html', error=error)
+        elif not content:
+            error = "내용을 입력해주세요."
+            return render_template('update.html', error=error)
+
+        models.update_Board(No, title, content)
+        return redirect(url_for('index'))
--- models.py
+++ models.py
@@ -3,7 +3,6 @@ from pymysql import cursors
 
 
 def get_connection():
-
     db_connection = pymysql.connect(
         host='localhost',
         user='root',
@@ -15,9 +14,9 @@ def get_connection():
 
     return db_connection, db_connection.cursor()
 
-# 모든 게시물
-def get_allBoards() :
 
+# 모든 게시물
+def get_allBoards():
     db_connection, cursor = get_connection()
 
     query = "SELECT * FROM Boards"
@@ -29,8 +28,9 @@ def get_allBoards() :
 
     return result
 
+
 # 게시물 찾기
-def get_Board(No):
+def get_board(No):
     db_connection, cursor = get_connection()
 
     # 튜플이 아닌 문자열이나 정수로 값을 넘기면 excute()는 시퀀스를 요구하기 때문에 에러발생
@@ -44,14 +44,51 @@ def get_Board(No):
 
     return result
 
+
+# 게시물 작성
 def create_Board(title, content, id):
     db_connection, cursor = get_connection()
 
     query = "INSERT INTO Boards (title, content, id) VALUES (%s, %s, %s)"
-    cursor.execute(query, (title,content,id))
+    cursor.execute(query, (title, content, id))
 
     db_connection.commit()
 
+    cursor.close()
+    db_connection.close()
+
+
+# 게시물 조회수 업데이트
+def update_views(No):
+    db_connection, cursor = get_connection()
+
+    query = "UPDATE Boards SET views = views + 1 WHERE No=%s"
+    cursor.execute(query, (No,))
+
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
+
+
+# 게시물 수정
+def update_Board(No, title, content):
+    db_connection, cursor = get_connection()
+
+    query = "UPDATE Boards SET title = %s, content = %s WHERE No=%s"
+    cursor.execute(query, (title, content, No))
+
+    db_connection.commit()
+    cursor.close()
+    db_connection.close()
+
+
+# 게시물 삭제
+def delete_Board(No):
+    db_connection, cursor = get_connection()
+    query = "DELETE FROM Boards WHERE No=%s"
+    cursor.execute(query, (No))
+
+    db_connection.commit()
     cursor.close()
     db_connection.close()
 
@@ -60,13 +97,14 @@ def get_User(id, pw):
     db_connection, cursor = get_connection()
 
     query = "SELECT * FROM Users WHERE id = %s and password = %s"
-    cursor.execute(query, (id,pw))
+    cursor.execute(query, (id, pw))
     result = cursor.fetchone()
 
     cursor.close()
     db_connection.close()
 
     return result
+
 
 # 회원가입
 def register(id, name, email, password):
@@ -76,7 +114,7 @@ def register(id, name, email, password):
     cursor.execute(query, (id,))
     result = cursor.fetchone()
 
-    if result :
+    if result:
         cursor.close()
         db_connection.close()
         return False
@@ -91,6 +129,3 @@ def register(id, name, email, password):
     db_connection.close()
 
     return True
-
-
-
